bac9c664bd3965d63aa6655501cdbf639b42ff7c
Igor Gonçalves <igordossantos38@gmail.com>
2025-10-24 08:25:06 -0300

    test: reformat AddToCartButton test for clarity and consistency

--- __tests__/AddToCartButton.test.tsx
+++ __tests__/AddToCartButton.test.tsx
@@ -18,23 +18,24 @@ describe('AddToCartButton Component', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
+
+  test.skip('dispara evento add_to_cart ao clicar', () => {
+    const amplitudeInitializer = AmplitudeInitializer.getInstance();
+  
+    const product = { 
+      id: '1', name: 
+      'Produto Teste', 
+      price: 49.9 
+    };
+  
+    render(<AddToCartButton product={product} />);
+    fireEvent.click(screen.getByTestId('add-to-cart'));
+  
+    expect(amplitudeInitializer.trackEvent).toHaveBeenCalledWith(EVENTS.ADD_TO_CART.name, {
+      [EVENTS.ADD_TO_CART.props.PRODUCT_ID]: '1',
+      [EVENTS.ADD_TO_CART.props.PRODUCT_NAME]: 'Produto Teste',
+      [EVENTS.ADD_TO_CART.props.PRICE]: 49.9,
+    });
+  });  
 });
 
-test.skip('dispara evento add_to_cart ao clicar', () => {
-  const amplitudeInitializer = AmplitudeInitializer.getInstance();
-
-  const product = { 
-    id: '1', name: 
-    'Produto Teste', 
-    price: 49.9 
-  };
-
-  render(<AddToCartButton product={product} />);
-  fireEvent.click(screen.getByTestId('add-to-cart'));
-
-  expect(amplitudeInitializer.trackEvent).toHaveBeenCalledWith(EVENTS.ADD_TO_CART.name, {
-    [EVENTS.ADD_TO_CART.props.PRODUCT_ID]: '1',
-    [EVENTS.ADD_TO_CART.props.PRODUCT_NAME]: 'Produto Teste',
-    [EVENTS.ADD_TO_CART.props.PRICE]: 49.9,
-  });
-});
