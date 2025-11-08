877e69ced955f2b8697b695a7f02669dd021e639
Igor Gonçalves <igordossantos38@gmail.com>
2025-11-08 14:23:31 -0300

    chore: update dependencies and configure Tailwind CSS and Stylelint
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Included Lucide React as a new dependency.
    - Updated package.json to include Stylelint standard configuration.
    - Modified vite.config.ts to integrate Tailwind CSS and Stylelint plugins.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [react()],
+        plugins: [
+            react(), 
+            tailwindcss(),
+        ],
         define: {
             'process.env': env,
         },
